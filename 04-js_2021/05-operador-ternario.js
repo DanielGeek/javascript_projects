da826d27d4d666e9f44fc7b3bdcc0206baf486df
DanielGeek <daniel30081990@gmail.com>
2021-02-21 20:36:39 -0300

    Operador Ternario en JavaScript () ? :

---
+++ 04-js_2021/05-operador-ternario.js
@@ -0,0 +1,29 @@
+const edad = 18
+let puedeManejar
+
+if (edad > 17) {
+  puedeManejar = 'Sí'
+} else {
+  puedeManejar = 'No'
+}
+
+console.log(puedeManejar)
+
+// condition ? expresion_1 : expresion_2
+let puedeManejar2 = edad > 17 ? (
+  console.log('Sí puedes manejar'), 'Sí'
+) : (
+    console.log('No puedes manejar'), 'No'
+  )
+console.log(puedeManejar2)
+
+const authenticated = 1
+const canEdit = authenticated === 1 ? true : false
+console.log(canEdit)
+const canEdit2 = authenticated === 1
+console.log(canEdit2)
+
+const speed = 240
+const message = speed > 180
+  ? 'Demasiado Rápido'
+  : (speed >= 110 ? 'Rápido' : 'Normal')
