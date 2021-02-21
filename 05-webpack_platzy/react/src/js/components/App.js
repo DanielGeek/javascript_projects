5748708def8dc8af26a25fefcd12c86b2ab47b65
DanielGeek <daniel30081990@gmail.com>
2021-02-21 16:08:21 -0300

    Trabajando con React

--- 05-webpack_platzy/react/src/js/components/App.js
+++ 05-webpack_platzy/react/src/js/components/App.js
@@ -1,10 +1,27 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './Loader'
+
+console.log(data)
 
 function App() {
+
+  const [loaderList, setLoaderList] = useState([])
+
+  const handleClick = () => {
+    setLoaderList(data.loaders)
+  }
+
   return (
     <div>
       que linda aplicación hecha en React.js
+      <ul>
+        {
+          loaderList.map(dataLoader => <Loader key={dataLoader.id} {...dataLoader} />)
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
     </div>
   )
 }
