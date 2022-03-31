17736abefcd0f4e81d7c809c1102c45df6b35590
DangelGlobant <daniel30081990@gmail.com>
2022-03-31 14:11:47 -0300

    vars and comments

---
+++ js-course-2022/02-vars-and-comments/02.1-example.js
@@ -0,0 +1,13 @@
+// this is a one line comment
+
+/*
+este es un comentario de varias lineas
+this is a multi line comment
+*/
+// with commas
+let a = 20, b = 20;
+let c = 20;
+let d = 20;
+let x = a + b;
+
+console.log(x);
