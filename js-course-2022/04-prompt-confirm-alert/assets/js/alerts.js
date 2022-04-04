fa305e95be04ab1fc1d5e87aea902def97a668c3
DangelGlobant <daniel30081990@gmail.com>
2022-04-04 11:18:34 -0400

    prom confirm and alert

---
+++ js-course-2022/04-prompt-confirm-alert/assets/js/alerts.js
@@ -0,0 +1,11 @@
+// alert('Hello');
+
+// let defaultName = prompt('Ingresa algo', 'valor por defecto');
+// console.log(defaultName);
+// console.log('***' + defaultName + '***');
+
+// const select = confirm('Quieres borrar?');
+// console.log(select);
+
+// objeto en node node 04-prompt-confirm-alert/assets/js/alerts.js
+console.log( global );
