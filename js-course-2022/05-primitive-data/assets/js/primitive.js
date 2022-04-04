8b82f3695e8c2a9d413d64d12bb7e9d385a545f3
DangelGlobant <daniel30081990@gmail.com>
2022-04-04 12:51:42 -0400

    General Introduction to Primitive Types

---
+++ js-course-2022/05-primitive-data/assets/js/primitive.js
@@ -0,0 +1,38 @@
+let myName = 'Daniel';
+console.log( myName );
+
+// ejemplo volviendo a declarar let myName
+// let myName = 'Batman';
+myName = 'Batman';
+console.log( myName );
+
+myName = "Jessica";
+myName = `otra forma de comillas`;
+
+console.log( typeof myName );
+
+myName = 123;
+console.log( typeof myName );
+
+let isMarvel = true;
+console.log( typeof isMarvel );
+
+let age = 31;
+console.log( typeof age );
+
+age = 31.001;
+console.log( typeof age );
+
+let superPower;
+console.log( typeof superPower );
+
+let iAmNull = null;
+console.log( typeof iAmNull );
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1 );
+console.log( symbol1 === symbol2 );
+
+
