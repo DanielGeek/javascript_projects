6373ab59ed943b9107999204045c6122de89d4af
DangelGlobant <daniel30081990@gmail.com>
2022-04-06 13:10:04 -0400

    More details on object literals

--- js-course-2022/08-literal-objects/assets/js/literal-objects.js
+++ js-course-2022/08-literal-objects/assets/js/literal-objects.js
@@ -1,4 +1,4 @@
-let character = {
+const character = {
   name: 'Bruce Wayne',
   codeName: 'Batman',
   alive: false,
@@ -8,7 +8,7 @@ let character = {
     lng: -118.70
   },
   suits: ['Batman Black', 'Batman Yellow', 'Batman Green'],
-  direction: {
+  address: {
     zip: '10880, 90265',
     location: 'Malibu, California'
   },
@@ -31,3 +31,39 @@ const x = 'alive';
 console.log('Alive', character[x]);
 
 console.log('Last Movie', character["last-movie"]);
+
+// more details
+// usando null mala practica
+// character.age = null;
+
+delete character.age;
+console.log(character);
+
+// otra forma de agregar un nuevo atributo al objeto
+character.married = true;
+
+// convertir obj a array interable
+const entriesPairs = Object.entries( character );
+console.log( entriesPairs );
+
+// ejemplo asignando al character const un nuevo valor para que de error
+// character = true;
+
+console.log( character );
+
+// bloquear el objeto para que no puedan asignarsele mas atributos o cambiarles los actuales de la primera gerarquia
+Object.freeze( character );
+
+character.money = 1000000;
+character.married = false;
+character.address.location = 'Venezuela';
+console.log( character );
+
+// obtener el nombre de las propiedades del objeto y convertirlo en array
+const properties = Object.getOwnPropertyNames( character );
+const values = Object.values( character );
+console.log({ properties });
+console.log({ values });
+
+// references
+// https://developer.mozilla.org/es/docs/web/javascript/reference/global_objects/object
