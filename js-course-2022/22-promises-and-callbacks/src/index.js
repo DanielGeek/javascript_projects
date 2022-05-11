284c38ba1df4279f1e7eca8c63a3bf76f5d477e4
DangelGlobant <daniel30081990@gmail.com>
2022-05-10 23:37:09 -0400

    126. Promises

--- js-course-2022/22-promises-and-callbacks/src/index.js
+++ js-course-2022/22-promises-and-callbacks/src/index.js
@@ -1,19 +1,27 @@
-import { buscarHeroe } from './js/callbacks';
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
+import { buscarHeroe } from './js/promesas';
+
 import './styles.css';
 
 const heroeId1 = 'capi';
 const heroeId2 = 'spider';
 
-buscarHeroe( heroeId1, ( err, heroe1 ) => {
+// buscarHeroe( heroeId1, ( err, heroe1 ) => {
 
-  if ( err ) { return console.error( err ); }
+//   if ( err ) { return console.error( err ); }
 
-  buscarHeroe( heroeId2, ( err, heroe2 ) => {
+//   buscarHeroe( heroeId2, ( err, heroe2 ) => {
 
-    if ( err ) { return console.error( err ); }
+//     if ( err ) { return console.error( err ); }
 
-    console.log(`Enviando a ${ heroe1.nombre } y ${ heroe2.nombre } a la misión`);
-  });
+//     console.log(`Enviando a ${ heroe1.nombre } y ${ heroe2.nombre } a la misión`);
+//   });
+
+// });
+
+buscarHeroe( heroeId1 ).then( heroe => {
+
+  console.log(`Enviando a ${ heroe.nombre } a la misión`);
 
 });
 
