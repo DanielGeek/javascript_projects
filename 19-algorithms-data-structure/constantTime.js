b59d0b52751c4ad1addf34367dd8753006c65f8d
Daniel <daniel.passage606@passmail.com>
2024-05-26 01:05:47 -0400

    33. O(1)

---
+++ 19-algorithms-data-structure/constantTime.js
@@ -0,0 +1,8 @@
+const boxesArray = [1, 2, 3, 4, 5];
+
+const compressFirstBox = (boxesArr) => {
+    console.log(boxesArr[0]);
+    console.log(boxesArr[1]);
+}
+
+compressFirstBox(boxesArray); // O(2) - Constant Time
